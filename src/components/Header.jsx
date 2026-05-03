6657d2b7e7dfaaefeef94b8996b33d100fbb591e
brunolimagit <bru_jefferson@hotmail.com>
2026-05-03 00:57:29 -0300

    criando nav do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,31 @@ import logoPatricia from '../assets/logo-patricia.png'
 
 export default function Header(){
     return(
-        <main className='bg-black'>
-            <header>
+        <main>
+            <header  className='flex justify-between px-10'>
 
                 {/*logo*/}
-                <div className=' w-[400px]'>
+                <div className=' w-[120px]'>
                     <img src={logoPatricia} className="" alt="logo-patricia" />
                 </div>
 
                {/*topicos*/}
-                <div></div>
+                <div  className="flex items-center">
+                    <nav>
+                        <ul className="flex gap-4 text-[pink] font-bold">
+                            <li className="hover:text-black"><a href="/">Inicio</a></li>
+                            <li><a href="/">Quem sou</a></li>
+                            <li><a href="/">Trabalhos</a></li>
+                            <li><a href="/">Especialidades</a></li>
+                            <li><a href="/">Fed Back</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
                 {/*login*/}
-                <div></div>
+                <div className="w-[120px]">
+                    <img src={logoPatricia} className="" alt="logo-patricia" />
+                </div>
             </header>
         </main>
     )
