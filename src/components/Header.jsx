6c610f25c84da3d9b4636b39885039896cf60c36
brunolimagit <bru_jefferson@hotmail.com>
2026-05-03 16:55:41 -0300

    atualizando style ao header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,19 @@ import logoPatricia from '../assets/logo-patricia.png'
 
 export default function Header(){
     return(
-        <main>
-            <header  className='flex justify-between px-10'>
+        <main className='bg-[#FFE2CE]'>
+            <header  className='flex justify-between px-10 items-center'>
 
                 {/*logo*/}
-                <div className=' w-[120px]'>
+                <div className='w-[100px]'>
                     <img src={logoPatricia} className="" alt="logo-patricia" />
                 </div>
 
                {/*topicos*/}
                 <div  className="flex items-center">
                     <nav>
-                        <ul className="flex gap-4 text-[pink] font-bold">
-                            <li className="hover:text-black"><a href="/">Inicio</a></li>
+                        <ul className="flex gap-4 text-[#D65FE4] font-bold text-xl">
+                            <li className="hover:text-[#D65FE4]"><a href="/">Inicio</a></li>
                             <li><a href="/">Quem sou</a></li>
                             <li><a href="/">Trabalhos</a></li>
                             <li><a href="/">Especialidades</a></li>
@@ -24,8 +24,14 @@ export default function Header(){
                 </div>
 
                 {/*login*/}
-                <div className="w-[120px]">
-                    <img src={logoPatricia} className="" alt="logo-patricia" />
+                <div className="flex gap-4">
+                    
+                    <button className="bg-[#DC96E4] text-white py-2 px-4 rounded-[50px] font-bold">
+                        Contado
+                    </button>
+                    <button className="bg-[#DC96E4] text-white py-2 px-4 rounded-[50px] font-bold">
+                        Login
+                    </button>
                 </div>
             </header>
         </main>
